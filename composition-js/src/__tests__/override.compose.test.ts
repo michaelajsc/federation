a5d1c068794d15c9c1314d92d0ba706b9a84051d
Trevor Scheer <trevor.scheer@gmail.com>
2024-01-02 09:10:44 -0800

    add reproduction

--- composition-js/src/__tests__/override.compose.test.ts
+++ composition-js/src/__tests__/override.compose.test.ts
@@ -502,7 +502,7 @@ describe("composition involving @override directive", () => {
 
     const result = composeAsFed2Subgraphs([subgraph1, subgraph2]);
     expect(result.errors).toBeDefined();
-    expect(result.errors?.map(e => e.message)).toMatchStringArray([
+    expect(result.errors?.map((e) => e.message)).toMatchStringArray([
       `
       The following supergraph API query:
       {
@@ -526,7 +526,7 @@ describe("composition involving @override directive", () => {
       - from subgraph "Subgraph2":
         - cannot find field "T.a".
         - cannot move to subgraph "Subgraph1" using @key(fields: "k") of "T", the key field(s) cannot be resolved from subgraph "Subgraph2" (note that some of those key fields are overridden in "Subgraph2").
-      `
+      `,
     ]);
   });
 
@@ -562,9 +562,9 @@ describe("composition involving @override directive", () => {
     expect(result.errors).toBeDefined();
     expect(errors(result)).toStrictEqual([
       [
-        'FIELD_TYPE_MISMATCH',
-        'Type of field "T.a" is incompatible across subgraphs: it has type "Int" in subgraph "Subgraph1" but type "String" in subgraph "Subgraph2"'
-      ]
+        "FIELD_TYPE_MISMATCH",
+        'Type of field "T.a" is incompatible across subgraphs: it has type "Int" in subgraph "Subgraph1" but type "String" in subgraph "Subgraph2"',
+      ],
     ]);
   });
 
@@ -810,7 +810,7 @@ describe("composition involving @override directive", () => {
   // At the moment, we've punted on @override support when interacting with @interfaceObject, so the
   // following tests mainly cover the various possible use and show that it currently correcly raise
   // some validation errors. We may lift some of those limitation in the future.
-  describe('@interfaceObject', () => {
+  describe("@interfaceObject", () => {
     it("does not allow @override on @interfaceObject fields", () => {
       // We currently rejects @override on fields of an @interfaceObject type. We could lift
       // that limitation in the future, and that would mean such override overrides the field
@@ -913,5 +913,109 @@ describe("composition involving @override directive", () => {
         'Invalid @override on field "A.a" of subgraph "Subgraph2": source subgraph "Subgraph1" does not have field "A.a" but abstract it in type "I" and overriding abstracted fields is not supported.',
       ]);
     });
+  });
+
+  it("TODO (repro)", () => {
+    const subgraph1 = {
+      name: "Subgraph1",
+      url: "https://Subgraph1",
+      typeDefs: gql`
+        type Query @shareable {
+          query: String! @override(from: "Subgraph3")
+        }
+      `,
+    };
+
+    const subgraph2 = {
+      name: "Subgraph2",
+      url: "https://Subgraph2",
+      typeDefs: gql`
+        type Query @shareable {
+          query: String! @override(from: "Subgraph4")
+        }
+      `,
+    };
+
+    const subgraph3 = {
+      name: "Subgraph3",
+      url: "https://Subgraph3",
+      typeDefs: gql`
+        type Query @shareable {
+          query: String!
+        }
+      `,
+    };
+
+    const subgraph4 = {
+      name: "Subgraph4",
+      url: "https://Subgraph4",
+      typeDefs: gql`
+        type Query @shareable {
+          query: String!
+        }
+      `,
+    };
+
+    const result = composeAsFed2Subgraphs([
+      subgraph1,
+      subgraph2,
+      subgraph3,
+      subgraph4,
+    ]);
+    expect(result.supergraphSdl).toMatchInlineSnapshot(`
+      "schema
+        @link(url: \\"https://specs.apollo.dev/link/v1.0\\")
+        @link(url: \\"https://specs.apollo.dev/join/v0.3\\", for: EXECUTION)
+      {
+        query: Query
+      }
+
+      directive @join__enumValue(graph: join__Graph!) repeatable on ENUM_VALUE
+
+      directive @join__field(graph: join__Graph, requires: join__FieldSet, provides: join__FieldSet, type: String, external: Boolean, override: String, usedOverridden: Boolean) repeatable on FIELD_DEFINITION | INPUT_FIELD_DEFINITION
+
+      directive @join__graph(name: String!, url: String!) on ENUM_VALUE
+
+      directive @join__implements(graph: join__Graph!, interface: String!) repeatable on OBJECT | INTERFACE
+
+      directive @join__type(graph: join__Graph!, key: join__FieldSet, extension: Boolean! = false, resolvable: Boolean! = true, isInterfaceObject: Boolean! = false) repeatable on OBJECT | INTERFACE | UNION | ENUM | INPUT_OBJECT | SCALAR
+
+      directive @join__unionMember(graph: join__Graph!, member: String!) repeatable on UNION
+
+      directive @link(url: String, as: String, for: link__Purpose, import: [link__Import]) repeatable on SCHEMA
+
+      scalar join__FieldSet
+
+      enum join__Graph {
+        SUBGRAPH1 @join__graph(name: \\"Subgraph1\\", url: \\"https://Subgraph1\\")
+        SUBGRAPH2 @join__graph(name: \\"Subgraph2\\", url: \\"https://Subgraph2\\")
+        SUBGRAPH3 @join__graph(name: \\"Subgraph3\\", url: \\"https://Subgraph3\\")
+        SUBGRAPH4 @join__graph(name: \\"Subgraph4\\", url: \\"https://Subgraph4\\")
+      }
+
+      scalar link__Import
+
+      enum link__Purpose {
+        \\"\\"\\"
+        \`SECURITY\` features provide metadata necessary to securely resolve fields.
+        \\"\\"\\"
+        SECURITY
+
+        \\"\\"\\"
+        \`EXECUTION\` features provide metadata necessary for operation execution.
+        \\"\\"\\"
+        EXECUTION
+      }
+
+      type Query
+        @join__type(graph: SUBGRAPH1)
+        @join__type(graph: SUBGRAPH2)
+        @join__type(graph: SUBGRAPH3)
+        @join__type(graph: SUBGRAPH4)
+      {
+        query: String! @join__field(graph: SUBGRAPH1, override: \\"Subgraph3\\") @join__field(graph: SUBGRAPH2, override: \\"Subgraph4\\")
+      }"
+    `);
+    expect(errors(result)).toMatchInlineSnapshot(`Array []`);
   });
 });
